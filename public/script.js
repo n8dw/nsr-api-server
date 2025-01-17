beae823127b0cf8f657d8c6583389f3f6a5676ed
Nathan <19847214+n8dw@users.noreply.github.com>
2025-01-17 01:38:48 -0500

    2.0.0 DEV – Continue

--- public/script.js
+++ public/script.js
@@ -1,15 +1,28 @@
-const userID = "auth0|6591609c687600590068d1f9"
-const response = fetch("./v2/accounts/get/", {
-    method: 'POST',
-    body: JSON.stringify({
-        userID: userID,
-    })
-});
+async function test() {
+    const userID = localStorage.getItem('a0UserId');
+    console.log('USERID:', userID);
+    const response = await fetch("http://localhost:3000/v2/accounts/get/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            a0UserId: userID,
+        })
+    });
 
-if (response.ok) {
-    const data = await response.json();
-    console.log(data);
-    document.getElementById("data_demo").innerHTML = JSON.stringify(data);
-} else {
-    console.error('Error:', response.statusText);
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        document.getElementById("data_demo").innerHTML = JSON.stringify(data);
+    } else {
+        console.error('Error:', response.statusText);
+    }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', async () => {
+        await test();
+    });
+});
--- server.js
+++ server.js
@@ -1,27 +1,8 @@
 const cors = require("cors");
 const express = require("express");
 const firebase = require("firebase-admin");
-const helmet = require("helmet");
 const app = express();
 const PORT = 3000;
-
-// Middleware
-app.use(
-    cors({
-        origin: ['http://localhost:3000', 'https://nightshade.red'],
-        credentials: true,
-    }),
-    helmet({
-        contentSecurityPolicy: {
-            directives: {
-            defaultSrc: ["'self'"],
-            connectSrc: ["'self'", "http://localhost:3000"], // Allow localhost connections
-            },
-        },
-    }),
-    // Allow anything in the /public directory
-    express.static('public')
-);
 
 // Initialize Firebase
 const servAcct = require("./fbsa.json");
@@ -34,6 +15,21 @@ firebase.initializeApp({
 // Initiate with CAN2-PROD database
 const firestore = firebase.firestore();
 
+// Middleware
+const allowedOrigins = ['http://localhost:3000', 'https://nightshade.red'];
+app.use(
+    // CORS
+    cors({
+        origin: allowedOrigins,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+    // Allow anything in the /public directory
+    express.static('public'),
+    // Parse JSON bodies
+    express.json(),
+);
+
 // Expose index.html
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -41,13 +37,20 @@ app.get("/", (req, res) => {
 
 /* ACCOUNTS */
 // 01 - Get Account
-app.get("/v2/accounts/get/", async (req, res) => {
+app.post("/v2/accounts/get/", async (req, res) => {
     // Allow POST requests from localhost
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    //res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    //res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    // Get the a0UserId from the request
-    const a0UserId = req.query.a0UserId;
+    // Get a0UserId from the request body
+    const { a0UserId } = req.body;
+
+    // If there's no a0UserId, return an error
+    if (!a0UserId) {
+        return res.status(400).json({
+            error: "No a0UserId provided",
+        });
+    }
 
     // Find a document in the accountDetail collection with an auth0UserId value of a0UserId
     // Don't send it back yet, there's more to do with the data
@@ -121,7 +124,186 @@ app.get("/v2/accounts/get/", async (req, res) => {
     });
 });
 
-// 02 - Generate API token
+/* BOOKMARK */
+// 01 – Get Entire Group
+app.post("/v2/bookmark/get/", async (req, res) => {
+    // Get the a0UserId and groupID from the request body
+    const { a0UserId, groupID } = req.body;
+
+    // If there's no a0UserId or groupID, return an error
+    if (!a0UserId || !groupID) {
+        return res.status(400).json({
+            error: "No a0UserId or groupID provided",
+        });
+    }
+
+    // Find a document in the accountDetail collection with an auth0UserId value of a0UserId
+    // Don't send it back yet, there's more to do with the data
+    var accountRaw = await firestore.collection("accountDetail")
+                    .where("auth0UserId", "==", a0UserId)
+                    .get();
+
+    // Get the account data
+    var account = accountRaw.docs[0].data();
+
+    // Get the group data
+    var groupData = await firestore.collection('athenaeum_roster')
+                    .where('atID', '==', groupID)
+                    .get();
+
+    // Check publicity / access
+    if (groupData.docs[0].data().user !== account.accountNumber) {
+        if (groupData.docs[0].data().access !== "public") {
+            if (groupData.docs[0].data().userGroup === accountDetail.docs[0].data().userGroup[0]) {
+                // Permitted
+            }
+            else {
+                // Return error
+                return res.status(403).json({
+                    error: "Access denied",
+                });
+            }
+        }
+    }
+
+    // Get the athenaeumData in the group
+    var athenaeumData = [];
+    for (let i = 0; i < groupData.docs[0].data().athenaeumData.length; i++) {
+        var recordData = await firestore.collection('athenaeum_data')
+                        .where('id', '==', groupData.docs[0].data().athenaeumData[i])
+                        .get();
+        athenaeumData.push(recordData.docs[0].data());
+    }
+
+    // Sort by title
+    athenaeumData.sort((a, b) => {
+        if (a.recordTitle < b.recordTitle) {
+            return -1;
+        }
+        if (a.recordTitle > b.recordTitle) {
+            return 1;
+        }
+        return 0;
+    });
+
+    
+    res.json({
+        athenaeumData,
+        access: groupData.docs[0].data().access,
+        atID: groupID,
+        iconLink: null,
+        title: groupData.docs[0].data().title,
+        type: groupData.docs[0].data().type,
+        showOnHub: groupData.docs[0].data().showOnHub,
+        user: groupData.docs[0].data().user,
+    });
+});
+
+// 02 - Create/Edit/Save Record
+app.post("/v2/bookmark/saverecord/", async (req, res) => {
+    // Get a0UserId, recordID, recordTitle (string), recordCategory (string), recordTags (array), recordLinkToData and group ID from the request body
+    const { a0UserId, recordID, recordTitle, recordCategory, recordTags, recordLinkToData, groupID } = req.body;
+
+    // If any are missing, return an error
+    if (!a0UserId || !recordID || !recordTitle || !recordCategory || !recordTags || !recordLinkToData || !groupID) {
+        return res.status(400).json({
+            error: "Missing data",
+        });
+    }
+
+    // Get the account data
+    var accountRaw = await firestore.collection("accountDetail")
+                    .where("auth0UserId", "==", a0UserId)
+                    .get();
+
+    // Get the account data
+    var account = accountRaw.docs[0].data();
+
+    // Get the group data
+    var groupData = await firestore.collection('athenaeum_roster')
+                    .where('atID', '==', groupID)
+                    .get();
+
+    // Check publicity / access
+    if (athenaeum.docs[0].data().access === "private" && athenaeum.docs[0].data().user !== accountNumber) {
+        // If the userGroup in the athenaeum and the user's userGroup do not match, and the user's athenaea array does not contain the athenaeumID, return an error
+        if ((athenaeum.docs[0].data().userGroup !== user.docs[0].data().userGroup[0]) && (user.docs[0].data().athenaea.includes(athenaeumID) === false)) {
+            return res.status(403).json({
+                error: "Access denied",
+            });
+        }
+    }
+    
+    // If the record does not exist create it.
+    // If the record does not exist, create a new record
+    else if (recordID === "" || recordID === null || recordID === undefined) {
+        console.log("NEW RECORD");
+        // Generate random 32-character string (A-Z, a-z, 0-9)
+        var chars =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var newRecordID = "";
+        for (var i = 0; i < 32; i++) {
+          newRecordID += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        console.log("NEW RECORD ID", newRecordID);
+        console.log("ADDING");
+        // Add the record to Firestore (athenaeum_data)
+        await db.collection("athenaeum_data").add({
+          recordCategory: body.recordCategory,
+          recordID: newRecordID,
+          recordLinkToData: body.recordLinkToData,
+          recordTags: body.recordTags,
+          recordTitle: body.recordTitle,
+        });
+        console.log("ADDED SUCCESSFULLY");
+        // Add the recordID to the athenaeum_roster
+        // Get the athenaeum from Firestore (athenaeum_roster; contains atID and an array of recordIDs)
+        console.log("UPDATING ROSTER");
+  
+        // Get the recordIDs from the athenaeum
+        let recordIDs = athenaeum.docs[0].data().recordIDs;
+        recordIDs.push(newRecordID);
+        // Update the athenaeum with the new recordID
+        await db.collection("athenaeum_roster").doc(athenaeum.docs[0].id).update({
+          recordIDs: recordIDs,
+        });
+        console.log("ROSTER UPDATED");
+        // Return a message and the entire record
+        return res.json({
+          message: "Record added successfully",
+          record: {
+            recordCategory: recordCategory,
+            recordID: newRecordID,
+            recordLinkToData: recordLinkToData,
+            recordTags: recordTags,
+            recordTitle: recordTitle,
+          },
+        });
+    }
+
+    // Otherwise, create a new record
+    else {
+        // Get the record from Firestore (athenaeum_data)
+        const record = await db
+          .collection("athenaeum_data")
+          .where("recordID", "==", recordID)
+          .get();
+        await db.collection("athenaeum_data").doc(record.docs[0].id).update({
+            recordTitle: body.recordTitle,
+            recordCategory: body.recordCategory,
+            recordTags: body.recordTags,
+            recordLinkToData: body.recordLinkToData,
+        });
+        return res.json({
+            message: "Record updated successfully",
+            recordCategory: recordCategory,
+            recordID: recordID,
+            recordLinkToData: recordLinkToData,
+            recordTags: recordTags,
+            recordTitle: recordTitle,
+        });
+    }
+});
 
 
 app.listen(PORT, () => {
